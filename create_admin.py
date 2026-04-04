d739b549aa390787ed15d4acc7082b4672b08cb4
Mutindaezie <113909547+Mutindaezie@users.noreply.github.com>
2026-04-04 15:16:36 +0300

    Update create_admin.py
    Django Initialization

--- create_admin.py
+++ create_admin.py
@@ -1,3 +1,10 @@
+import os
+import django
+
+# 🔧 Initialize Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hustler_Fund_loan_Default_Prediction.settings')
+django.setup()
+
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
